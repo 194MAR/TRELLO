9977560d4716ac37ec298fd02dfdd45ac507dcc8
194MAR <bellarodriguez36@gmail.com>
2018-05-17 18:40:47 -0500

    version 0.0.2

---
+++ js/README.md
@@ -0,0 +1,2 @@
+En esta primero version se  muestra el texto añadir un lista
+y al hacer click ocultarlo.
---
+++ js/app.js
@@ -0,0 +1,28 @@
+/*var list = document.getElementsByClassName("add-list");
+list.addEventListener('click',hiddig)
+function hiddig(event){
+    list.style.display='none';
+};*/
+/*var addList = document.getElementById("anadir-lista");
+function hiddig(event){
+var cuadroGris = document.getElementById("cuadro-gris");
+}*/
+
+var buttonList = document.getElementById("agregar-lista");
+buttonList.addEventListener("click", showGreySquare);
+function showGreySquare(){
+    var greySquare = document.getElementById("cuadro-gris");
+    var addList = document.getElementById("anadir-lista");
+    greySquare.style.visibility="visible";
+    addList.style.visibility="hidden";
+} 
+var saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click",newList);
+function newList(){
+    var newListInputValue = document.getElementById("newListInput").value;
+    var listTitle = document.getElementById("list-title");
+    listTitle.value = newListInputValue;
+    document.getElementById("newListInput").value=" ";
+    var cardColum = document.getElementById("cardColum");
+    cardColum.style.display="block";
+}
