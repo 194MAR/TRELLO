d61de20f0ebabcaf03567950fee1c31bd85cfe6d
Squallac <bellarodriguez36@gmail.com>
2018-05-17 20:51:10 -0500

    version final

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-/*var list = document.getElementsByClassName("add-list");
-list.addEventListener('click',hiddig)
-function hiddig(event){
-    list.style.display='none';
-};*/
-/*var addList = document.getElementById("anadir-lista");
-function hiddig(event){
-var cuadroGris = document.getElementById("cuadro-gris");
-}*/
-
 var buttonList = document.getElementById("agregar-lista");
 buttonList.addEventListener("click", showGreySquare);
 function showGreySquare(){
@@ -20,7 +10,7 @@ var saveButton = document.getElementById("saveButton");
 saveButton.addEventListener("click",newList);
 function newList(){
     var newListInputValue = document.getElementById("newListInput").value;
-    var listTitle = document.getElementById("list-title");
+    var listTitle = document.getElementById("listTitle");
     listTitle.value = newListInputValue;
     document.getElementById("newListInput").value=" ";
     var cardColum = document.getElementById("cardColum");
@@ -32,8 +22,17 @@ function showContet(){
     newCardButton.style.display="none";
     var addCardForm= document.getElementById("cuadroGris2")
     addCardForm.style.display="block";
-    
+    var listTitle = document.getElementById("listTitle");
+    listTitle.focus();
 
+}
 
+var saveCardButton = document.getElementById("saveCardButton");
+saveCardButton.addEventListener("click",addNewCard);
+function addNewCard(){
+    var cardsValue = document.getElementById("cardsValue").value;
+    var card = document.getElementById("card");
+    card.innerHTML=cardsValue;
+    card.style.display="block";
 
 }
